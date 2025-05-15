edb5faf2abbf4e260b1d7559d138b12db66a0cb9
Srikanth Subramanian <sri.subra@hurix.com>
2025-05-14 20:39:57 -0500

    fix: remove unused Image import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from 'next/navigation';
 import { getServerSession } from 'next-auth';
-import Image from "next/image";
 
 export default async function Home() {
   const session = await getServerSession();
